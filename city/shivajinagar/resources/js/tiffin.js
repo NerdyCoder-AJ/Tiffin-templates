1e04726a311630d09e297700ec565db15dcca560
akash <akash.codeway@gmail.com>
2020-07-29 11:25:14 +0530

    Enter

---
+++ city/shivajinagar/resources/js/tiffin.js
@@ -0,0 +1,30 @@
+  <script type="text/javascript">
+
+
+      // button section
+
+      function call_btn()
+{
+      alert("9970184493");
+}
+      function call_btn1()
+{
+      alert("8208987119");
+}
+      function call_btn2()
+{
+      alert("9146392902");
+}
+      function call_btn3()
+{
+      alert("9503654754");
+}
+      function call_btn4()
+{
+      alert("7020663642");
+}
+
+
+</script>
+
+
